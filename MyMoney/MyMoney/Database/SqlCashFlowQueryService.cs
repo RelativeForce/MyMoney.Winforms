1ebf6ce32bb3a68dc8fe035e49459563c003a55a
RelativeForce <joshiceddy@hotmail.com>
2018-08-28 22:20:44 +0100

    Formatting

--- MyMoney/MyMoney/Database/SqlCashFlowQueryService.cs
+++ MyMoney/MyMoney/Database/SqlCashFlowQueryService.cs
@@ -4,83 +4,83 @@ using MyMoney.Core.Model;
 
 namespace MyMoney.Database
 {
-   public class SqlCashFlowQueryService : ICashFlowQueryService
-   {
+    public class SqlCashFlowQueryService : ICashFlowQueryService
+    {
 
-      private const string DateFormat = "{0}{1}{2}{3}-{4}{5}-{6}{7}";
+        private const string DateFormat = "{0}{1}{2}{3}-{4}{5}-{6}{7}";
 
-      public Command SelectAllBetween(DateTime start, DateTime end)
-      {
-         var endMonthString = end.ToShortDateString();
+        public Command SelectAllBetween(DateTime start, DateTime end)
+        {
+            var endMonthString = end.ToShortDateString();
 
-         var startDateString = start.ToShortDateString();
-         
-         var startMonth = string.Format(DateFormat,
-            startDateString[6],
-            startDateString[7],
-            startDateString[8],
-            startDateString[9],
-            startDateString[3],
-            startDateString[4],
-            "0", "1");
+            var startDateString = start.ToShortDateString();
 
-         var endMonth = string.Format(DateFormat,
-            endMonthString[6],
-            endMonthString[7],
-            endMonthString[8],
-            endMonthString[9],
-            endMonthString[3],
-            endMonthString[4],
-            "0", "1");
+            var startMonth = string.Format(DateFormat,
+               startDateString[6],
+               startDateString[7],
+               startDateString[8],
+               startDateString[9],
+               startDateString[3],
+               startDateString[4],
+               "0", "1");
 
-         var queryText =
-            $"SELECT * FROM {CashFlowModel.TABLE_NAME} WHERE {CashFlowModel.DATE_COLOUMN} BETWEEN '{startMonth}' AND '{endMonth}' ORDER BY {CashFlowModel.DATE_COLOUMN} DESC;";
+            var endMonth = string.Format(DateFormat,
+               endMonthString[6],
+               endMonthString[7],
+               endMonthString[8],
+               endMonthString[9],
+               endMonthString[3],
+               endMonthString[4],
+               "0", "1");
 
-         return new Command(queryText);
-      }
+            var queryText =
+               $"SELECT * FROM {CashFlowModel.TABLE_NAME} WHERE {CashFlowModel.DATE_COLOUMN} BETWEEN '{startMonth}' AND '{endMonth}' ORDER BY {CashFlowModel.DATE_COLOUMN} DESC;";
 
-      public Command Delete(string id)
-      {
-         var commandText = $"DELETE FROM {CashFlowModel.TABLE_NAME} WHERE {CashFlowModel.TRANSACTION_ID_COLOUMN} = {id}";
+            return new Command(queryText);
+        }
 
-         return new Command(commandText);
-      }
+        public Command Delete(string id)
+        {
+            var commandText = $"DELETE FROM {CashFlowModel.TABLE_NAME} WHERE {CashFlowModel.TRANSACTION_ID_COLOUMN} = {id}";
 
-      public Command Update(string id, string newValue, string updatedCol)
-      {
+            return new Command(commandText);
+        }
 
-         var command =
-            $"UPDATE {CashFlowModel.TABLE_NAME} SET {updatedCol} = {newValue} WHERE {CashFlowModel.TRANSACTION_ID_COLOUMN} = {id};";
+        public Command Update(string id, string newValue, string updatedCol)
+        {
 
-         return new Command(command);
+            var command =
+               $"UPDATE {CashFlowModel.TABLE_NAME} SET {updatedCol} = {newValue} WHERE {CashFlowModel.TRANSACTION_ID_COLOUMN} = {id};";
 
-      }
+            return new Command(command);
 
-      public Command Add(string id, string date, string amount, string description)
-      {
+        }
 
-         var formattedDate = string.Format(DateFormat,
-            date[6],
-            date[7],
-            date[8],
-            date[9],
-            date[3],
-            date[4],
-            date[0],
-            date[1]);
+        public Command Add(string id, string date, string amount, string description)
+        {
 
-         var commandText = $"INSERT INTO CashFlow VALUES ('{id}', '{description}', '{formattedDate}', {amount})";
+            var formattedDate = string.Format(DateFormat,
+               date[6],
+               date[7],
+               date[8],
+               date[9],
+               date[3],
+               date[4],
+               date[0],
+               date[1]);
 
-         return new Command(commandText);
+            var commandText = $"INSERT INTO CashFlow VALUES ('{id}', '{description}', '{formattedDate}', {amount})";
 
-      }
+            return new Command(commandText);
 
-      public Command Create()
-      {
-         var commandText =
-            $"CREATE TABLE {CashFlowModel.TABLE_NAME}({CashFlowModel.TRANSACTION_ID_COLOUMN} NUMBER(10,0) UNIQUE, {CashFlowModel.DESCRIPTION_COLOUMN} VARCHAR({CashFlowModel.DESCRIPTION_LENGTH}) NOT NULL, {CashFlowModel.DATE_COLOUMN} DATE NOT NULL, {CashFlowModel.AMOUNT_COLOUMN} NUMBER(5,2) NOT NULL, PRIMARY KEY ({CashFlowModel.TRANSACTION_ID_COLOUMN}));";
+        }
 
-         return new Command(commandText);
-      }
-   }
+        public Command Create()
+        {
+            var commandText =
+               $"CREATE TABLE {CashFlowModel.TABLE_NAME}({CashFlowModel.TRANSACTION_ID_COLOUMN} NUMBER(10,0) UNIQUE, {CashFlowModel.DESCRIPTION_COLOUMN} VARCHAR({CashFlowModel.DESCRIPTION_LENGTH}) NOT NULL, {CashFlowModel.DATE_COLOUMN} DATE NOT NULL, {CashFlowModel.AMOUNT_COLOUMN} NUMBER(5,2) NOT NULL, PRIMARY KEY ({CashFlowModel.TRANSACTION_ID_COLOUMN}));";
+
+            return new Command(commandText);
+        }
+    }
 }
