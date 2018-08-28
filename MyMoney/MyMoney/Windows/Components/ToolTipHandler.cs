7bef569f548a58ae4b6813868ff95b0fca750bbd
RelativeForce <joshiceddy@hotmail.com>
2018-08-28 22:26:05 +0100

    Formatting

--- MyMoney/MyMoney/Windows/Components/ToolTipHandler.cs
+++ MyMoney/MyMoney/Windows/Components/ToolTipHandler.cs
@@ -4,65 +4,65 @@ using System.Windows.Forms;
 namespace MyMoney.Windows.Components
 {
 
-   public class ToolTipHandler
-   {
-      private ToolTip _toolTip;
+    public class ToolTipHandler
+    {
+        private ToolTip _toolTip;
 
-      private ToolTipModel _model;
+        private ToolTipModel _model;
 
-      public ToolTipHandler()
-      {
-         _toolTip = null;
-         _model = null;
-      }
+        public ToolTipHandler()
+        {
+            _toolTip = null;
+            _model = null;
+        }
 
-      public void Draw(ToolTipModel model)
-      {
+        public void Draw(ToolTipModel model)
+        {
 
-         if (model == null) throw new Exception("ToolTip cannot be null");
+            if (model == null) throw new Exception("ToolTip cannot be null");
 
-         if (_model == null) Show(model);
+            if (_model == null) Show(model);
 
-         var same = _model.Equals(model);
+            var same = _model.Equals(model);
 
-         if (same) return;
+            if (same) return;
 
-         var sameAnchor = _model.SameAnchor(model);
+            var sameAnchor = _model.SameAnchor(model);
 
-         if (sameAnchor)
-         {
-            AlterVisible(model);
-         }
-         else
-         {
-            Swap(model);
-         }
-      }
+            if (sameAnchor)
+            {
+                AlterVisible(model);
+            }
+            else
+            {
+                Swap(model);
+            }
+        }
 
-      private void Swap(ToolTipModel model)
-      {
-         _toolTip.Hide(_model.Anchor);
+        private void Swap(ToolTipModel model)
+        {
+            _toolTip.Hide(_model.Anchor);
 
-         Show(model);
-      }
+            Show(model);
+        }
 
-      private void Show(ToolTipModel model)
-      {
-         _model = model;
+        private void Show(ToolTipModel model)
+        {
+            _model = model;
 
-         var newToolTip = new ToolTip { ToolTipTitle = _model.Title };
-         newToolTip.Show(_model.Message, _model.Anchor);
+            var newToolTip = new ToolTip { ToolTipTitle = _model.Title };
+            newToolTip.Show(_model.Message, _model.Anchor);
 
-         _toolTip = newToolTip;
+            _toolTip = newToolTip;
 
-      }
+        }
 
-      private void AlterVisible(ToolTipModel model)
-      {
-         _model = model;
-         _toolTip.Show(_model.Message, _model.Anchor);
-      }
+        private void AlterVisible(ToolTipModel model)
+        {
+            _model = model;
+            _toolTip.Show(_model.Message, _model.Anchor);
+        }
 
-   }
+    }
 
 }
