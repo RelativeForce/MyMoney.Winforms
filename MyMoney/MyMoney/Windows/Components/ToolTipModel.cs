6c332f231a49cb2beefddd30fa7120fdb2eed657
RelativeForce <joshiceddy@hotmail.com>
2018-08-28 22:26:25 +0100

    Formatting

--- MyMoney/MyMoney/Windows/Components/ToolTipModel.cs
+++ MyMoney/MyMoney/Windows/Components/ToolTipModel.cs
@@ -2,32 +2,32 @@
 
 namespace MyMoney.Windows.Components
 {
-   public class ToolTipModel 
-   {
-      public readonly Control Anchor;
+    public class ToolTipModel
+    {
+        public readonly Control Anchor;
 
-      public readonly string Title;
+        public readonly string Title;
 
-      public readonly string Message;
+        public readonly string Message;
 
-      public ToolTipModel(string title, string message, Control anchor)
-      {
-         Anchor = anchor;
-         Title = title;
-         Message = message;
-      }
+        public ToolTipModel(string title, string message, Control anchor)
+        {
+            Anchor = anchor;
+            Title = title;
+            Message = message;
+        }
 
-      public bool Equals(ToolTipModel toCheck)
-      {
-         var sameTitle = Title.Equals(toCheck.Title);
-         var sameMessage = Message.Equals(toCheck.Message);
+        public bool Equals(ToolTipModel toCheck)
+        {
+            var sameTitle = Title.Equals(toCheck.Title);
+            var sameMessage = Message.Equals(toCheck.Message);
 
-         return SameAnchor(toCheck) && sameTitle && sameMessage;
-      }
+            return SameAnchor(toCheck) && sameTitle && sameMessage;
+        }
 
-      public bool SameAnchor(ToolTipModel toCheck)
-      {
-         return Anchor.Equals(toCheck.Anchor);
-      }
-   }
+        public bool SameAnchor(ToolTipModel toCheck)
+        {
+            return Anchor.Equals(toCheck.Anchor);
+        }
+    }
 }
