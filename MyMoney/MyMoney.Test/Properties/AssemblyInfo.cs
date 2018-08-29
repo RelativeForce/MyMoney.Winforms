bee68db5da0421b71f38dad97b25a5d5dec34b39
Joshua Eddy <joshiceddy@hotmail.com>
2018-08-29 14:40:18 +0100

    Added Test project

---
+++ MyMoney/MyMoney.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyMoney.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMoney.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b28af41d-6bc4-44c7-ab35-d8524be81f52")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyMoney/MyMoney.Test/RowTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyMoney.Test
+{
+   [TestClass]
+   public class RowTest
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+      }
+   }
+}
