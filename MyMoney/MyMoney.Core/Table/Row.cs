9665a544333dc7545c1b17317ca39613e218bd6e
Joshua Eddy <joshiceddy@hotmail.com>
2018-08-30 10:07:19 +0100

    Added Row.Equals()

--- MyMoney/MyMoney.Core/Table/Row.cs
+++ MyMoney/MyMoney.Core/Table/Row.cs
@@ -34,5 +34,50 @@ namespace MyMoney.Core.Table
             return _rawRow[coloumn];
         }
 
-    }
+        public override bool Equals(object o)
+        {
+            if (!(o is Row)) return false;
+            var row = (Row) o;
+
+            return IsSameAsThis(row);
+        }
+
+        private bool IsSameAsThis(Row row)
+        {
+            return HasSameColoumNames(row) && HasSameColoumValues(row);
+        }
+
+        private bool HasSameColoumValues(Row row)
+        {
+            foreach (var coloumn in _rawRow.Keys)
+            {
+                if (!_rawRow[coloumn].Equals(row._rawRow[coloumn]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool HasSameColoumNames(Row row)
+        {
+
+            var coloumnChecklist = _rawRow.Keys.ToList();
+
+            foreach (var column in row._rawRow.Keys)
+            {
+ 
+                if (coloumnChecklist.Contains(column))
+                {
+                     coloumnChecklist.Remove(column);
+                }
+                else
+                {
+                     return false;
+                }
+            }
+
+          return coloumnChecklist.Count == 0;
+        }
+   }
 }
