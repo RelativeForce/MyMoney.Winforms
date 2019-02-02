7123b873c33ae8ac0821eeced13f5af11d03cd76
RelativeForce <joshiceddy@hotmail.com>
2019-02-02 15:06:39 +0000

    Added unit tests

--- MyMoney/MyMoney.Infrastructure.Tests/RepositoryShould.cs
+++ MyMoney/MyMoney.Infrastructure.Tests/RepositoryShould.cs
@@ -1,9 +1,7 @@
 ﻿using MyMoney.Infrastructure.Repositories;
-using System;
 using Xunit;
 using System.Data.Entity;
 using Moq;
-using MyMoney.Core.Data;
 using MyMoney.Infrastructure.Interfaces;
 
 namespace MyMoney.Infrastructure.Tests
@@ -11,41 +9,94 @@ namespace MyMoney.Infrastructure.Tests
 
     public class RepositoryShould
     {
-        private const double TestAmount = 32.2;
-        private const string TestDescription = "test description";
 
         [Fact]
-        public void ShouldAddWhenEntityIsNotNull()
+        public void ShouldAddWhenEntityToAddIsNotNull()
         {
             const int testId = 14;
-            const int testSaveResult = 5;
 
-            var testTransaction = new Transaction {
+            var testObject = new TestObject
+            {
                 Id = testId,
-                Date = DateTime.Today,
-                Amount = TestAmount,
-                Description = TestDescription
             };
 
-            var moqDbSet = new Mock<DbSet<Transaction>>();
-            moqDbSet.Setup(mdbs => mdbs.Add(testTransaction)).Returns(testTransaction);
+            var moqDbSet = new Mock<DbSet<TestObject>>();
+            moqDbSet.Setup(mdbs => mdbs.Add(testObject)).Returns(testObject);
 
             var moqDbContext = new Mock<IDatabaseContext>();
-            moqDbContext.Setup(mdbc => mdbc.Transactions).Returns(moqDbSet.Object);
-            moqDbContext.Setup(mdbc => mdbc.SaveChanges()).Returns(testSaveResult);
 
-            var testRepository = new TestRepository<Transaction>(moqDbContext.Object, moqDbSet.Object);
+            var testRepository = new TestRepository<TestObject>(moqDbContext.Object, moqDbSet.Object);
 
-            var result = testRepository.Add(testTransaction);
+            var result = testRepository.Add(testObject);
 
             Assert.NotNull(result);
-            Assert.Equal(result.Id, testTransaction.Id);
-            Assert.Equal(result.Date, testTransaction.Date);
-            Assert.Equal(result.Amount, testTransaction.Amount);
-            Assert.Equal(result.Description, testTransaction.Description);
+            Assert.Equal(result.Id, testObject.Id);
 
             moqDbContext.Verify(mdbc => mdbc.SaveChanges(), Times.Once);
+            moqDbSet.Verify(mdbs => mdbs.Add(testObject), Times.Once);
 
+        }
+
+        [Fact]
+        public void ShouldReturnNullWhenEntityToAddIsNull()
+        {
+            var moqDbSet = new Mock<DbSet<TestObject>>();
+
+            var moqDbContext = new Mock<IDatabaseContext>();
+
+            var testRepository = new TestRepository<TestObject>(moqDbContext.Object, moqDbSet.Object);
+
+            var result = testRepository.Add(null);
+
+            Assert.Null(result);
+            moqDbContext.Verify(mdbc => mdbc.SaveChanges(), Times.Never);
+            moqDbSet.Verify(mdbs => mdbs.Add(null), Times.Never);
+        }
+
+        [Fact]
+        public void ShouldRemoveWhenEntityToAddIsNotNull()
+        {
+            const int testId = 14;
+
+            var testObject = new TestObject
+            {
+                Id = testId,
+            };
+
+            var moqDbSet = new Mock<DbSet<TestObject>>();
+            
+            var moqDbContext = new Mock<IDatabaseContext>();
+
+            var testRepository = new TestRepository<TestObject>(moqDbContext.Object, moqDbSet.Object);
+
+            var result = testRepository.Delete(testObject);
+
+            Assert.True(result);
+            moqDbContext.Verify(mdbc => mdbc.SaveChanges(), Times.Once);
+            moqDbSet.Verify(mdbs => mdbs.Remove(testObject), Times.Once);
+
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenEntityToRemoveIsNull()
+        {
+            var moqDbSet = new Mock<DbSet<TestObject>>();
+
+            var moqDbContext = new Mock<IDatabaseContext>();
+
+            var testRepository = new TestRepository<TestObject>(moqDbContext.Object, moqDbSet.Object);
+
+            var result = testRepository.Delete(null);
+
+            Assert.False(result);
+            moqDbContext.Verify(mdbc => mdbc.SaveChanges(), Times.Never);
+            moqDbSet.Verify(mdbs => mdbs.Remove(null), Times.Never);
+        }
+
+
+        public class TestObject
+        {
+            public int Id { get; set; }
         }
 
         private class TestRepository<T> : Repository<T> where T : class
